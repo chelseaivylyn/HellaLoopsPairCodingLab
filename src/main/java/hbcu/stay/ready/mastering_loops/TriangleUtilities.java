abb31de8b8e63e67c3fe28d9ca4cbe5f4b3a217e
Chelsea Osei Mensah <66383441+chelseaivylyn@users.noreply.github.com>
2020-07-07 19:36:11 -0400

    completed loops

--- src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
+++ src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
@@ -2,19 +2,41 @@ package hbcu.stay.ready.mastering_loops;
 
 public class TriangleUtilities {
 
+    
     public static String getTriangle(int numberOfRows) {
-        return null;
+        StringBuilder newStr = new StringBuilder();
+        for(int i=1; i<numberOfRows; i++){
+            String star = "*";
+            newStr.append(star.repeat(i));
+            newStr.append("\n");    
+        }
+        return newStr.toString();
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        StringBuilder newStr = new StringBuilder();
+            String star = "*";
+            newStr.append(star.repeat(numberOfStars));  
+            return newStr.toString();
     }
 
     public static String getSmallTriangle() {
-        return null;
+        StringBuilder newStr = new StringBuilder();
+        for(int i=1; i<5; i++){
+            String star = "*";
+            newStr.append(star.repeat(i));
+            newStr.append("\n");    
+        }
+        return newStr.toString();
     }
 
     public static String getLargeTriangle() {
-        return null;
+        StringBuilder newStr = new StringBuilder();
+        for(int i=1; i<10; i++){
+            String star = "*";
+            newStr.append(star.repeat(i));
+            newStr.append("\n");    
+        }
+        return newStr.toString();
     }
 }
